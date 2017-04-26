319241c3c6802cdcd8257a78c28889ea6412589c
Sanju <sanjuap800@gmail.com>
2017-04-26 14:45:03 +0530

    Making changes in userdetailsController.js and checking in

--- controllers/client/userdetailsController.js
+++ controllers/client/userdetailsController.js
@@ -5,13 +5,12 @@ app.controller('submitForml',function($scope,$http,$location, savedService){
   $scope.save = function(){
     console.log($scope.form); 
 
-
-    if($scope.userForm.$valid)
+	if($scope.userForm.$valid)
 	    $http.post('/userdetails', $scope.form).then(function(res){
 	      console.log(res);
 	      savedService.set(res.data);
 	      $location.path('/checkout');
 	    });
-  };
+	};
 
 });
