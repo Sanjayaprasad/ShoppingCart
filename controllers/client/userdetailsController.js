655c65681a6dcba8a700ed8912f3cd24e0b0b2b3
Sanju <sanjuap800@gmail.com>
2017-05-03 14:00:06 +0530

    Making changes in userdetailsController.js and checking in

--- controllers/client/userdetailsController.js
+++ controllers/client/userdetailsController.js
@@ -1,10 +1,14 @@
 //Submit controller for userDetails
-app.controller('submitForml',function($scope,$http,$location, savedService){
+app.controller('submitForml',function($scope,$http,$location, savedService, myService){
 	//$scope.onlyNumbers = /^\d+$/;
 	// $scope.form = { };
+	$scope.back = function(){
+    	$location.path('/home');
+    }
+
   $scope.save = function(){
     console.log($scope.form); 
-
+	myService.setData($scope.form);
 	if($scope.userForm.$valid)
 	    $http.post('/userdetails', $scope.form).then(function(res){
 	      console.log(res);
@@ -14,3 +18,26 @@ app.controller('submitForml',function($scope,$http,$location, savedService){
 	};
 
 });
+
+// app.controller('submitForml',function($scope,$http,$location, savedService, myService){
+//  //$scope.onlyNumbers = /^\d+$/;
+//  // $scope.form = { };
+
+//     $scope.back = function(){
+//       $location.path('/home');
+//     }
+
+//   $scope.form = myService.getData();
+  
+//   $scope.save = function(){
+//     console.log($scope.form); 
+//         myService.setData($scope.form);
+//  if($scope.userForm.$valid)
+//      $http.post('/userdetails', $scope.form).then(function(res){
+//        console.log(res);
+//        savedService.set(res.data);
+//        $location.path('/checkout');
+//      });
+//  };
+
+// });
