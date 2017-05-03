8e0b919cfc323361019adcab7a4c02d595abfb7a
Sanju <sanjuap800@gmail.com>
2017-05-03 15:02:46 +0530

    Adding jasmineReportGenerator and checking in

--- test/conf.js
+++ test/conf.js
@@ -1,8 +1,18 @@
 exports.config = {
-  framework: 'jasmine',
+  framework: 'jasmine2',
  
   specs: ['spec.js'],
   capabilities: {
   	browserName: 'chrome'
+  },
+
+  onPrepare: function(){
+  	require('jasmine-reporters');
+  	jasmine.getEnv().addReporter(
+  		new jasmine.JUnitXmlReporter({
+  			consolidateAll : true,
+  			savePath: 'test',
+  			filePrefix: 'xmloutput'
+  	}));
   }
 }
